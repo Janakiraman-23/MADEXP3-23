c33b23801c7b035ac2acadce3789ccd72beac5e2
Uday kiran <221701062@rajalakshmi.edu.in>
2025-05-04 10:49:35 +0530

    INIT COMMITT

---
+++ app/src/main/java/com/example/a3exp23/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.graphicalprimitives
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.a3exp23.R
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
